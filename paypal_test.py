1d601904e0feaf1cba2347e7b42ea5e82247da81
kadish-apiiro <david.kadish@apiiro.com>
2025-10-16 13:26:55 +0300

    Update paypal_test.py

--- paypal_test.py
+++ paypal_test.py
@@ -1,3 +1,38 @@
 import paypalrestsdk
+import logging
 
-payment = paypalrestsdk.Payment()
+paypalrestsdk.configure({
+  "mode": "sandbox", # sandbox or live
+  "client_id": "EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM",
+  "client_secret": "EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM" })
+
+payment = paypalrestsdk.Payment({
+  "intent": "sale",
+  "payer": {
+    "payment_method": "credit_card",
+    "funding_instruments": [{
+      "credit_card": {
+        "type": "visa",
+        "number": "4417119669820331",
+        "expire_month": "11",
+        "expire_year": "2018",
+        "cvv2": "874",
+        "first_name": "Joe",
+        "last_name": "Shopper" }}]},
+  "transactions": [{
+    "item_list": {
+      "items": [{
+        "name": "item",
+        "sku": "item",
+        "price": "1.00",
+        "currency": "USD",
+        "quantity": 1 }]},
+    "amount": {
+      "total": "1.00",
+      "currency": "USD" },
+    "description": "This is the payment transaction description." }]})
+
+if payment.create():
+  print("Payment created successfully")
+else:
+  print(payment.error)
